ce0d72b7aca6bf34b5c638c4d64c27375af88c92
patrick.norris <patrick.norris@gitlab.k8s.cloud.statcan.ca>
2019-06-20 10:34:28 -0400

    aria activedescendant test

--- src/pointerMixin.js
+++ src/pointerMixin.js
@@ -109,15 +109,17 @@ export default {
     },
     pointerReset () {
       /* istanbul ignore else */
+      alert('yolo')
       if (!this.closeOnSelect) return
       this.pointer = 0
-      alert('heyo')
-      for(var i = 0; i < this.$refs.listbox.childNodes.length; ++i){
-        if(this.$refs.listbox.childNodes[i].getAttribute('role') == 'option'){
-          alert('yolo')
-          this.$refs.search.setAttribute('aria-activedescendant', this.$refs.listbox.childNodes[i].getAttribute('id'))
-        }
-      }
+      this.$refs.search.setAttribute('aria-activedescendant', this.id + '-1'))
+      //alert('heyo')
+      //for(var i = 0; i < this.$refs.listbox.childNodes.length; ++i){
+        //if(this.$refs.listbox.childNodes[i].getAttribute('role') == 'option'){
+          //alert('yolo')
+          //this.$refs.search.setAttribute('aria-activedescendant', this.$refs.listbox.childNodes[i].getAttribute('id'))
+        //}
+      //}
       /* istanbul ignore else */
       if (this.$refs.list) {
         this.$refs.list.scrollTop = 0
