b30f8030e07d57ba7164cd826549cb5ccb526370
patrick.norris <patrick.norris@gitlab.k8s.cloud.statcan.ca>
2019-06-21 08:51:48 -0400

    aria-activedescendant test

--- src/Multiselect.vue
+++ src/Multiselect.vue
@@ -66,7 +66,7 @@
           @keydown.delete.stop="removeLastElement()"
           class="multiselect__input"
           :aria-controls="'listbox-'+id"
-          :aria-activedescendant=""
+          aria-activedescendant=""
         />
         <span
           v-if="isSingleLabelVisible"
